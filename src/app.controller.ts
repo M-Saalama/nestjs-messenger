5b096685d1ba50b5ee611e741c8a18fa2a27ff5a
M-Saalama <mohamed.salama@crowdanalyzer.com>
2025-04-09 14:10:10 +0200

    testing the structure

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { SqsService } from './sqs/sqs.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly configService: ConfigService) {
+  constructor(private readonly configService: ConfigService , @Inject('SQS_SERVICE_TEST1') private readonly sqsService1: SqsService, @Inject('SQS_SERVICE_TEST2') private readonly sqsService2: SqsService) {
     const number = this.configService.get<number>('PORT');
     console.log('number', number);
   }
 
   @Get()
   getHello(): string {
+    this.sqsService1.printUrl();
+    this.sqsService2.printUrl();
     return "Hello World!";
   }
 }
--- src/sqs/sqs.service.ts
+++ src/sqs/sqs.service.ts
@@ -5,4 +5,7 @@ export class SqsService {
     constructor(private queueConfig: SqsModuleConfig) {
         this.queueConfig = queueConfig;
     }
+    printUrl() {
+        console.log(this.queueConfig.queueUrl);
+    }
 }
